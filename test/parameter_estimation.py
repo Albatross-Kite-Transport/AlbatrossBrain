775229c4adfbbf04e09aec2144f55bb0cc5931f0
1-Bart-1 <bart@vandelint.net>
2024-07-19 08:08:11 +0200

    good parameter estimation

--- test/parameter_estimation.py
+++ test/parameter_estimation.py
@@ -4,8 +4,15 @@ from scipy.integrate import solve_ivp
 from scipy.optimize import least_squares
 import matplotlib.pyplot as plt
 
-measurement = pd.read_csv("./measurement_data.csv")
-V = 3.3
+measurement = pd.read_csv("./measurement_data4.csv")
+sm_diff = measurement['sm'].diff()
+time_diff = measurement['time'].diff()
+measurement['speed'] = sm_diff / time_diff / 48
+measurement['speed'].fillna(0, inplace=True)
+print(measurement.head())
+
+speed_name = 'speed'
+V = 1.0
 
 def system(t, y, b, J, K, L, R):
     theta_dot, i = y
@@ -16,8 +23,11 @@ def system(t, y, b, J, K, L, R):
 def simulate(params):
     b, J, K, L, R = params
     y0 = [0, 0]  # Initial values for theta' and i
-    t_span = (0, 10)  # From t=0 to t=10
-    sol = solve_ivp(system, t_span, y0, t_eval=np.linspace(t_span[0], t_span[1], 66), args=(b, J, K, L, R))
+    # t_span = (0, 10)  # From t=0 to t=10
+    t_eval = measurement['time'].values  # Use the time column from measurement DataFrame
+    t_span = (t_eval[0], t_eval[-1])  # From the first to the last time point
+# np.linspace(t_span[0], t_span[1], 66)
+    sol = solve_ivp(system, t_span, y0, t_eval=t_eval, args=(b, J, K, L, R))
     return sol
 
 def objective(params, plot=False):
@@ -28,35 +38,34 @@ def objective(params, plot=False):
         'i': sol.y[1]
     })
     area = 0.0
-    for (i, theta_dot) in enumerate(sim['theta_dot']):
-        if np.isfinite(measurement['v'][i]):
-            area += theta_dot - measurement['v'][i]
+    for (i, theta_dot) in enumerate(sim['theta_dot'][1:]):
+        if measurement['time'][i] < 0.2:
+            area += abs(theta_dot - measurement[speed_name][i])
+        else:
+            area += abs(theta_dot - measurement[speed_name][i])
     if plot:
-        sim.plot(y='theta_dot')
-        measurement.plot(y='v')
+        plt.figure()
+        plt.plot(sim['time'], sim['theta_dot'], label='Simulated theta_dot')
+        plt.plot(measurement['time'], measurement[speed_name], label='Measured v')
+        # plt.plot(measurement['time'], measurement['speed'], label='Calc v')
+        plt.xlabel('Time')
+        plt.ylabel('Value')
+        plt.legend()
         plt.show()
+        # sim.plot(y='theta_dot')
+        # measurement.plot(y=speed_name)
+        # plt.show()
     print(area)
+    if area == 0:
+        area = 100
     return area
 
-initial_guess = [1.5319e-04, 1.2130e-05, 0.0034, 0.0127, 0.0710]
-result = least_squares(objective, initial_guess)
-optimized_params = result.x
-print(f'Optimized parameters: {optimized_params}')
+# b, J, K, L, R
+params = [0.08682405, 0.00258706, 0.11394443, 0.04805594, 0.07610658]
+
+result = least_squares(objective, params, max_nfev=10000, ftol=1e-6, xtol=1e-6, gtol=1e-6)
+params = result.x
+print(f'Optimized parameters: {params}')
 
 # Simulate with the optimized parameters
-objective(optimized_params, plot=True)
-# sim = pd.DataFrame({
-#     'time': sol.t,
-#     'theta_dot': sol.y[0],
-#     'i': sol.y[1]
-# })
-
-# # Plot the results
-# plt.figure()
-# plt.plot(sim['time'], sim['theta_dot'], label='Simulated theta_dot')
-# plt.plot(measurement['time'], measurement['theta_dot'], label='Measured theta_dot')
-# plt.legend()
-# plt.xlabel('Time')
-# plt.ylabel('theta_dot')
-# plt.title('Comparison of Simulated and Measured theta_dot')
-# plt.show()
+objective(params, plot=True)
